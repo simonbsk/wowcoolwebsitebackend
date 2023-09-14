c8798e9e5695fa70d0ed05c83b27194aab0d1d7e
a19c704 <simon.batschkuhn@rewe-group.com>
2023-09-14 11:31:45 +0200

    initial commit

---
+++ wowcoolwebsitebackend/src/main/java/com/wowcoolwebsitebackend/WowcoolwebsitebackendApplication.java
@@ -0,0 +1,13 @@
+package com.wowcoolwebsitebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowcoolwebsitebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WowcoolwebsitebackendApplication.class, args);
+	}
+
+}
---
+++ wowcoolwebsitebackend/src/test/java/com/wowcoolwebsitebackend/WowcoolwebsitebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wowcoolwebsitebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowcoolwebsitebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
