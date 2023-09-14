a8ef99a14570d549fbf6b18fe881fe55500d1188
simonbsk <97104622+simonbsk@users.noreply.github.com>
2023-09-14 11:36:16 +0200

    Delete coolwebsitebackend/src/main/java/com/example/demo directory

--- coolwebsitebackend/src/main/java/com/example/demo/classes/chart/ChartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.classes.chart;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChartRepository extends JpaRepository<Chart, Long> {
-}
